51727afbd5c48e40dcc03a8306f04ced8c136457
boss <fepsilux@yahoo.com>
2017-04-22 18:13:25 +0700

    22apr

--- _plugins/category_archive_plugin.rb
+++ _plugins/category_archive_plugin.rb
@@ -89,7 +89,7 @@ module Jekyll
       self.ext = '.html'
       self.basename = 'index'
       self.content = <<-EOS
-{% for post in page.posts %}<li><a href="{{ post.url | prepend: site.baseurl | replace: '//', '/' }}"><span>{{ post.title }}<span></a></li>
+{% for post in page.posts %}<li class="archi"><a href="{{ post.url | prepend: site.baseurl | replace: '//', '/' }}"><span>{{ post.title }}<span></a></li>
 {% endfor %}
       EOS
       self.data = {
